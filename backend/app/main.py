d9469da908933ba765995b373b423b88a9315347
Krishna <your_email@example.com>
2026-08-19 15:38:37 +0530

    cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,20 +20,13 @@ origins = [
     "http://127.0.0.1:5174",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://researchub-blue.vercel.app"
+    "https://researchub-blue.vercel.app",
+    "https://researchhub-blue.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://researchhub-blue.vercel.app",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
